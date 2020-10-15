ca7b616a5de7189924c7d1b8f2fd4eb0c0634dce
FrancoBester <61046880+FrancoBester@users.noreply.github.com>
2020-10-15 12:40:07 +0200

    item_pages & navigation

--- MVC/NeonTrees/NeonTrees/Controllers/HomeController.cs
+++ MVC/NeonTrees/NeonTrees/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace NeonTrees.Controllers
             return View();
         }
 
+        public IActionResult Options()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ MVC/NeonTrees/NeonTrees/Controllers/ItemsController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NeonTrees.Models;
+
+namespace NeonTrees.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly ILogger<ItemsController> _logger;
+
+        public ItemsController(ILogger<ItemsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Case()
+        {
+            return View();
+        }
+
+        public IActionResult Cooler()
+        {
+            return View();
+        }
+
+        public IActionResult Cpu()
+        {
+            return View();
+        }
+        public IActionResult Gpu()
+        {
+            return View();
+        }
+        public IActionResult Hdd()
+        {
+            return View();
+        }
+        public IActionResult Mother()
+        {
+            return View();
+        }
+        public IActionResult Psu()
+        {
+            return View();
+        }
+        public IActionResult Ram()
+        {
+            return View();
+        }
+        public IActionResult Ssd()
+        {
+            return View();
+        }
+
+    }
+}
