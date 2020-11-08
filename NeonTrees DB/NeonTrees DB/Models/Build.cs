5089556cbb9faf7d02e1a5973cc7523dafc0a196
Enrico <forty5degreesdev@gmail.com>
2020-11-08 21:01:17 +0200

    Added migration
    Connectionstring doesnt work

---
+++ NeonTrees DB/NeonTrees DB/Models/Build.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeonTrees_DB.Models
+{
+    public class Build
+    {
+        public int buildID { get; set; }
+        public DateTime buildDate { get; set; }
+        public int customerID { get; set; }
+        public double total { get; set; }
+    }
+}
---
+++ NeonTrees DB/NeonTrees DB/Models/login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeonTrees_DB.Models
+{
+    public class login
+    {
+        [Key]
+        public int loginID { get; set; }
+        public int customerID { get; set; }
+        public string password { get; set; }
+    }
+}
