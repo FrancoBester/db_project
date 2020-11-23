7bc79910c533342af8c08e8d38ba8d991f9e4b78
John King <Johnk858@ymail.com>
2020-11-24 00:33:30 +0200

    Simple Oracle Connection (Console App)
    Simple 3 field table tested with ONE of these inside of TNSNAMES.ora
    //My SID
    ORCL =
      (DESCRIPTION =
        (ADDRESS = (PROTOCOL = TCP)(HOST = localhost)(PORT = 1521))
        (CONNECT_DATA =
          (SERVER = DEDICATED)
          (SERVICE_NAME = orcl)
        )
      )
      ///// Enrico tyoe
      XE =
    (DESCRIPTION =
    (ADDRESS = (PROTOCOL = TCP)(HOST = TESTING.COM)(PORT = 1521))
    (CONNECT_DATA =
    (SERVER = DEDICATED)
    (SERVICE_NAME = XE)
    )

---
+++ Simple Oracle Connection (Console App)/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conString = "Data Source=localhost:1521/ORCL;User Id=SYSTEM;Password=Operator12;";
+
+            using (OracleConnection con = new OracleConnection(conString))
+            {
+                using OracleCommand cmd = con.CreateCommand();
+                try
+                {
+                    con.Open();
+                    cmd.BindByName = true;
+
+                    //use the table
+                    cmd.CommandText = "select * from EMPLOYEE";
+
+                    //assign id to 50
+                    OracleParameter id = new OracleParameter("id", 50);
+                    cmd.Parameters.Add(id);
+
+                    //execute the cmd and use DataReader
+                    OracleDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("Some data e.g employee Name: " + reader.GetString(0));
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine("Press enter to continue");
+
+                    reader.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
